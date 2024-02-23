6b8e029e01828a9a4f94d8b2438cd7ec2edf185c
noumankhandatu <noumankhan619.915@gmail.com>
2024-02-24 00:20:02 +0500

    test

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,16 +1,55 @@
 import Box from "@mui/system/Box";
 import CssBaseline from "@mui/material/CssBaseline";
 import Typography from "@mui/material/Typography";
-import { Button } from "@mui/material";
+import { Button, IconButton, Paper } from "@mui/material";
 import AppMap from "./../components/map";
 import AppDrawer from "./../components/drawer";
 import SearchableSelect from "../components/atom/SearchSelect";
 import SearchOrderNumber from "../components/atom/SearchOrderNo";
 import { useSelector } from "react-redux";
 import { selectDriverOrderId } from "../toolkit/slices/DriverOrderId";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 const Dashboard = () => {
+  const authToken = useSelector((state) => state.auth.token);
+  const [allRoutesData, setAllRoutesData] = useState(null);
   const Id = useSelector(selectDriverOrderId);
+
+  useEffect(() => {
+    const fetchAllRoutes = async () => {
+      try {
+        const response = await axios.get(
+          "https://portal.reliabletiredisposalhq.com/api/all-routes",
+          {
+            headers: {
+              Authorization: `Bearer ${authToken}`,
+            },
+          }
+        );
+        console.log(response, "response");
+        setAllRoutesData(response?.data?.data);
+      } catch (error) {
+        console.error("Error fetching all routes:", error);
+      }
+    };
+
+    fetchAllRoutes();
+  }, []);
+  const handleDeleteRoute = async (id) => {
+    try {
+      await axios.get(`https://portal.reliabletiredisposalhq.com/api/delete-route/${id}`, {
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+        },
+      });
+      // Remove the deleted route from the list
+      setAllRoutesData((prevRoutes) => prevRoutes.filter((route) => route.id !== id));
+    } catch (error) {
+      console.error("Error deleting route:", error);
+    }
+  };
 
   return (
     <div>
@@ -50,6 +89,22 @@ const Dashboard = () => {
           <Box height="40px" />
           <AppMap />
         </Box>
+      </Box>
+      <Box sx={{ ml: 35 }}>
+        All Routes
+        {allRoutesData &&
+          allRoutesData.map((route) => (
+            <Paper
+              elevation={3}
+              sx={{ p: 2, mb: 3, display: "flex", justifyContent: "space-between" }}
+              key={route.id}
+            >
+              {route.route_name}
+              <IconButton sx={{ textAlign: "end" }} onClick={() => handleDeleteRoute(route.id)}>
+                <DeleteIcon sx={{ color: "red" }} />
+              </IconButton>
+            </Paper>
+          ))}
       </Box>
     </div>
   );
